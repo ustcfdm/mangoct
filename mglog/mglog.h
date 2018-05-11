2fda35c379f43ed8915f8214f6fbd558c353b6e7
Mango <mango.feng@wisc.edu>
2018-05-10 20:38:36 -0500

    mglog: add function LoadConfigFile(..) [to be finished]

--- mglog/mglog.h
+++ mglog/mglog.h
@@ -2,6 +2,52 @@
 
 #include "stdafx.h"
 
+struct Config
+{
+	/*********************************************************
+	* input and out director and files
+	*********************************************************/
 
+	std::string		inputDir;
+	std::string		outputDir;
+
+	std::string		bkgFile;
+	std::vector<std::string>	inputFiles;
+
+	std::string		outputFilePrefix;
+	std::string		outputFilePostfix;
+	std::vector<std::string>	outputFileReplace;
+
+	/*********************************************************
+	* sinogram and slice parameters
+	*********************************************************/
+
+	unsigned		bkgViews;
+	unsigned		objViews;
+
+	unsigned		rebinSize;
+	unsigned		sliceNumber;
+	unsigned		sliceStartIdx;
+	unsigned		sliceThickness;
+
+	/*********************************************************
+	* correct sinogram artifacts
+	*********************************************************/
+
+	bool		interpolateWhiteLines;
+	bool		smoothoutMarginalData;
+	unsigned	smoothoutLeftIdx;
+	unsigned	smoothoutRightIdx;
+
+	/*********************************************************
+	*  EVI file parameters, usually you do not need to change
+	*********************************************************/
+
+	unsigned	detectorWidth;
+	unsigned	detectorHeight;
+	unsigned	offsetToFirstImage;
+	unsigned	gap;
+
+}config;
 
 
